40d91ab14d88a3e30575bc701348db7ce3edb97d
BlackMoonPhantom <nathan.tandon@gmail.com>
2023-05-12 09:34:20 -0400

    fixed

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -38,7 +38,7 @@ function ProductDetail() {
 
   return (
     <div>
-      <h2>{product.name}</h2>
+      <h2>{product.title}</h2>
       <img src={product.image} alt={product.name} />
       <p>{product.description}</p>
       <p>{product.price}</p>
